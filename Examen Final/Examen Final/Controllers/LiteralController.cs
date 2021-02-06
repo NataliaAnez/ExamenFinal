bb9a2c32f34b1184a985bfdb3fcda03abd3644df
diegomisak <47564984+diegomisak@users.noreply.github.com>
2021-02-05 20:37:19 -0400

    Repositorio, proyecto y controlador creados.

---
+++ Examen Final/Examen Final/Controllers/LiteralController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examen_Final.Controllers
+{
+    public class LiteralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
